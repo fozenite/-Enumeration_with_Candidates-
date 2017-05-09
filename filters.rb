cc11753be478913ca12064ea43d8002ce6f928b0
Rohit Moza <moza.rohit@gmail.com>
2017-05-09 00:25:59 +0000

    All completed and figured out how to sort in hashes

--- filters.rb
+++ filters.rb
@@ -3,14 +3,30 @@
 
 def find(id)
   # Your code Here
+  @candidates.each do |candidate|
+    if candidate[:id] == id
+      return candidate
+    end
+  end
+  return
 end
 
 def experienced?(candidate)
   # Your code Here
+
+  return candidate[:years_of_experience] >=2
 end
 
 def qualified_candidates(candidates)
   # Your code Here
+  candidates.each do |candidate|
+    if (experienced?(candidate)) && (candidate[:github_points] >= 100) && (candidate[:languages].include?('Ruby') || candidate[:languages].include?('Python')) && (candidate[:date_applied] >= 15.days.ago.to_date) && (candidate[:age] > 17)
+      puts "#{candidate}"
+    end
+  end
 end
 
 # More methods will go below
+def ordered_by_qualifications(candidatesCollection)
+  return candidatesCollection.sort_by{ |hsh| [-hsh[:years_of_experience], -hsh[:github_points]]}
+end
